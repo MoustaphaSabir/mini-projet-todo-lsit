61e54d7e03dafc9646e6651c72b7ff32ac568e26
Moustapha Sabir <moustaphasabirmoussa61@gmail.com>
2024-04-16 14:37:40 +0200

    fichier index

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 
 <?php
+    //Paramétres de connection
     $serveur = "localhost";
     $utilisateur = "root";
     $mot_de_passe = "";
@@ -14,19 +15,22 @@
     }
 
     // Traiter les données du formulaire pour ajouter une nouvelle tâche
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if ($_POST["REQUEST_METHOD"] == "POST") {
         if (!empty($_POST['ajouttache'])) {
             $nouvelle_tache = $_POST['ajouttache'];
             // Insérer la nouvelle tâche dans la base de données
             $sql = "INSERT INTO liste (nom_tache) VALUES ('$nouvelle_tache')";
-            echo $sql;
+            // echo $sql;
             mysqli_query($connexion, $sql);
         }
     }
 
     // Récupérer les tâches depuis la base de données
-    $sql = "SELECT * FROM liste";
-      // Vérifier si la requête a réussi
+    
+    $sql = "SELECT * FROM liste ORDER BY id_tache DESC";
+
+    // Vérifier si la requête a réussi
     // echo "<ul>";
     //     if ($listes) {
     //         // print_r($cours);
@@ -39,10 +43,9 @@
     //         echo "Erreur : " . mysqli_error($connexion);
     //     }
     $resultat = mysqli_query($connexion, $sql);
-?>
 
-<!DOCTYPE html>
-<html lang="en">
+?><!DOCTYPE html>
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -51,14 +54,15 @@
 <body>
     <h1>TODO-LIST</h1>
     <form action="" method="post">
-        <?php
-            // Afficher les tâches récupérées depuis la base de données
-            while ($row = mysqli_fetch_assoc($resultat)) {
-                echo '<input type="checkbox" name="' . $row['nom_tache'] . '">';
-                echo $row['nom_tache'] . "<br>";
-            }
-        ?>
-        <button type="submit">Enregistrer</button>
+
+    <?php
+    foreach ($resultat as $row) {
+        echo '<input type="checkbox" name="' . $row['nom_tache'] . '">';
+        echo $row['nom_tache'] . "<br>";
+    }
+    ?>  <br>
+     
+        <button type="submit" name = "formulaireTache">Enregistrer</button>
     </form>
 
     <h2>Liste des tâches enregistrées</h2> 
